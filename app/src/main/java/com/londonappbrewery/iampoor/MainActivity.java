cdb34648c859846a63dde6d4313a7f4cb6b5edf9
Yungjae Shin <yungjae.shin@ybrain.com>
2017-10-09 17:20:32 +0900

    Challenge Completed

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -1,8 +1,9 @@
 package com.londonappbrewery.iampoor;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void coalClicked(View view){
+        Toast.makeText(view.getContext(), R.string.clicked, Toast.LENGTH_SHORT).show();
     }
 }
